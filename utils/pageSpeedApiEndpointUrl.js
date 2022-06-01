a374b2eb0804c024c5102a8613c07b60fbad15cb
Jamie Davis <jamiedavis@rentalcars.com>
2022-06-01 15:19:33 +0100

    fixed api key issue

--- utils/pageSpeedApiEndpointUrl.js
+++ utils/pageSpeedApiEndpointUrl.js
@@ -5,12 +5,13 @@ function pageSpeedApiEndpointUrl(strategy) {
   const rentalCarsWebsite =
     "https://www.rentalcars.com/search-results?cor=gb&doDay=12&doHour=10&doMinute=0&doMonth=7&doYear=2022&driversAge=30&ftsType=A&location=37761&locationName=Gatwick%20Airport&puDay=11&puHour=10&puMinute=0&puMonth=7&puYear=2022";
 
+  // TODO: add psi api key to .env
   return (
     apiBaseUrl +
     "?url=" +
     rentalCarsWebsite +
     "&key=" +
-    process.env.PSI_APIKEY +
+    "AIzaSyAjhQiehQvb6QZHTXZfaSoryUCCNwegX_w" +
     "&strategy=" +
     strategy
   );
